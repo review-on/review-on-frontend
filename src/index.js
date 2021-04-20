5813480983e9b6a0abfdeeb35123c546e2f9510e
jd053303 <jd053303@gmail.com>
2021-04-20 15:15:27 +0900

    chart test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BarChart from './Barchart';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BarChart />
   </React.StrictMode>,
   document.getElementById('root')
 );
