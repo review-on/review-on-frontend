9dfdd29d13e01bec7824906b7d645cac2afe49b8
jd053303 <jd053303@gmail.com>
2021-04-22 15:27:43 +0900

    login_join page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BarChart from './Barchart';
-import Graph from './Graph';
+import FormPage from './pages/FormPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BarChart />
-    <Graph />
+    <FormPage></FormPage>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
