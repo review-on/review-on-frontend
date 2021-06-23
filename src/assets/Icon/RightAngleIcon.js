95a80ccdfd151ff501e1fe63c6bb448850245095
shc0801 <shc123987@naver.com>
2021-06-24 00:15:13 +0900

    [ADD] <Icon> RightAngleIcon

---
+++ src/assets/Icon/RightAngleIcon.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+function SvgComponent(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={20}
+      height={15}
+      viewBox="0 0 26.749 14.999"
+      {...props}
+    >
+      <path
+        data-name="\uD328\uC2A4 24"
+        d="M26.13.599a2 2 0 010 2.892L14.868 14.4a2.16 2.16 0 01-2.986 0L.619 3.492A2 2 0 01.619.6a2.161 2.161 0 012.986 0l9.77 9.462L23.145.6A2.16 2.16 0 0126.13.599z"
+        fillRule="evenodd"
+        stroke="#fff"
+        strokeWidth={0}
+      />
+    </svg>
+  )
+}
+
+export default SvgComponent
