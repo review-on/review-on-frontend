d1b54630043d3046e3ab0fd9d5eafeeaf3d3cd84
shc0801 <shc123987@naver.com>
2021-06-23 11:39:57 +0900

    [MODIFY] pages -> containers

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   MainContainer,
-} from './pages';
+} from './containers';
 
 
 const Router: React.FC = () => {
