9ed07b8f6bb3c13090b9ee824832261afcfd0d3c
shc0801 <shc123987@naver.com>
2021-06-24 00:21:21 +0900

    [MODIFY] RightAngleIcon width change

--- src/assets/Icon/RightAngleIcon.js
+++ src/assets/Icon/RightAngleIcon.js
@@ -4,7 +4,7 @@ function SvgComponent(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={20}
+      width={18}
       height={15}
       viewBox="0 0 26.749 14.999"
       {...props}
