9b2ea6288e3aabc6f835c4b8f7a79f8f60a5c73d
shc0801 <shc123987@naver.com>
2021-06-23 14:35:49 +0900

    [ADD] MainPage

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,17 +4,22 @@ import { Header } from "../../component";
 
 import {dark, light} from "../../theme"; // 환경별 테마 정보 가져오기
 import { ThemeProvider } from 'styled-components';
-
-const [themeMode, setThemeMode] = useState('light'); 
-const theme = themeMode === 'light' ? light : dark; 
-
-const toggleTheme = () => setThemeMode(themeMode === 'light' ? 'dark' : 'light');
+import { useEffect } from 'react';
 
 const MainPage: React.FC = () => {
+  const [themeMode, setThemeMode] = useState('dark'); 
+  const theme = themeMode === 'light' ? light : dark; 
+  
+  const changeTheme = () => setThemeMode(themeMode === 'light' ? 'dark' : 'light');
+
+  useEffect(() => {
+    console.log(theme);
+  })
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Header></Header>
+        <Header changeTheme={changeTheme}></Header>
       </ThemeProvider>
     </>
   );
